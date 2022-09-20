1a65a4afe31f6e113ceeb3cad665feae88b90575
mia1234gh <bantians@163.com>
2022-09-20 20:32:00 +0800

    mini-dice-game

---
+++ src/Rolldice.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import Die from './Die';
+import './Rolldice.css';
+
+class Rolldice extends Component {
+    static defaultProps = {
+        sides:['one', 'two', 'three', 'four', 'five', 'six']
+    };
+
+    constructor(props) {
+        super(props);
+        this.state = { die1: "one", die2: "one",rolling:false };
+        this.roll = this.roll.bind(this);
+    }
+    roll() {
+        //pick 2 rolls,using index
+        const newDie1 = this.props.sides[Math.floor(Math.random() * this.props.sides.length)];
+        const newDie2 = this.props.sides[Math.floor(Math.random() * this.props.sides.length)];
+        //set state with new rolls
+        this.setState({ die1: newDie1, die2: newDie2, rolling: true });
+        // wait 
+        setTimeout(() => {
+            this.setState({ rolling: false })
+        }, 1000);
+    }
+
+    render() {
+        return (
+            <div className='Rolldice'>
+                <div className='Rolldice_container'>
+                    <Die face={this.state.die1} rolling={this.state.rolling} />
+                    <Die face={this.state.die2} rolling={this.state.rolling } />
+                </div>
+                <button onClick={this.roll} disabled={this.state.rolling}>
+                    {this.state.rolling?"Rolling...":"Roll dice!"}
+                </button>
+            </div>
+        )
+    }
+}
+
+export default Rolldice;
